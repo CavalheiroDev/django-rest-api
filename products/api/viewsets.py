c6a2e1c8f68c5ef9276d3d28378358ab2d4c5e17
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2021-11-01 23:00:04 -0300

    Fix: adicionando serializer_class

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,10 +1,12 @@
 from products.api.serializers import ProductSerializer
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
 from products.models import Product
 
 
 class ProductViewSet(GenericViewSet):
+
+    serializer_class = ProductSerializer
 
     def get_queryset(self, code=None):
         queryset = Product.objects.all()
