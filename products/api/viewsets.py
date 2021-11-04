2c938e3e482ea01b2a1d5469e97dadae84ceeca2
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2021-11-03 21:56:01 -0300

    Feat: adicionando metodo create da View

--- products/api/viewsets.py
+++ products/api/viewsets.py
@@ -1,18 +1,26 @@
-from products.api.serializers import ProductSerializer
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework import status
+
+from products.api.serializers import ProductSerializer
 from products.models import Product
 
 
-class ProductViewSet(GenericViewSet):
+class ProductViewSet(ModelViewSet):
 
     serializer_class = ProductSerializer
 
-    def get_queryset(self, code=None):
+    def get_queryset(self, pk=None):
         queryset = Product.objects.all()
-        if code:
-            queryset = Product.objects.filter(code=code)
+        if pk:
+            queryset = Product.objects.filter(code=pk)
         return queryset
 
     def create(self, request):
-        print(request.data)
+        product = request.data
+
+        serializer = self.serializer_class(data=product)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
