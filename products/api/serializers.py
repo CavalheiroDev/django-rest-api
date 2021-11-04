87e878f3bc8403b7e47b31c47b3612d50a7cd1a1
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2021-11-03 21:54:24 -0300

    Feat: adicionando product_category e tratamento na criação do produto

--- products/api/serializers.py
+++ products/api/serializers.py
@@ -3,7 +3,30 @@ from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_category = serializers.ChoiceField(
+        choices=Product.PRODUCT_CATEGORY, required=False)
 
     class Meta:
         model = Product
         fields = '__all__'
+
+    def create(self, validated_data: dict):
+
+        if 'product_category' in validated_data:
+            product = Product.objects.create(**validated_data)
+        elif validated_data.get('price') <= 100:
+            product = Product.objects.create(code=validated_data.get('code'),
+                                             name=validated_data.get('name'), description=validated_data.get('description'),
+                                             price=validated_data.get('price'), stock=validated_data.get('stock'),
+                                             unit=validated_data.get('unit'), product_category='B')
+        elif validated_data.get('price') <= 500:
+            product = Product.objects.create(code=validated_data.get('code'),
+                                             name=validated_data.get('name'), description=validated_data.get('description'),
+                                             price=validated_data.get('price'), stock=validated_data.get('stock'),
+                                             unit=validated_data.get('unit'), product_category='M')
+        elif validated_data.get('price') > 500:
+            product = Product.objects.create(code=validated_data.get('code'),
+                                             name=validated_data.get('name'), description=validated_data.get('description'),
+                                             price=validated_data.get('price'), stock=validated_data.get('stock'),
+                                             unit=validated_data.get('unit'), product_category='C')
+        return product
--- products/models.py
+++ products/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 
 class Product(models.Model):
+
+    PRODUCT_CATEGORY = (
+        ('B', 'Barato'),
+        ('M', 'Médio'),
+        ('C', 'Caro')
+    )
+
     code = models.CharField(primary_key=True, unique=True, max_length=10)
     name = models.CharField(max_length=60)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
     unit = models.CharField(max_length=10)
+    product_category = models.CharField(
+        max_length=10, choices=PRODUCT_CATEGORY, blank=True, null=True)
 
     class Meta:
         db_table = 'Product'
