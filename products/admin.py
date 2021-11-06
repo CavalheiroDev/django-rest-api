a389749b001cbd732de7aeea98217345d9cacef0
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2021-11-05 21:26:32 -0300

    Feat: criando classe admin para Product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.db.models import fields
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'price',
+                    'stock', 'unit', 'product_category')
+    list_display_links = ('code', 'name')
+    list_filter = ('code', 'name', 'product_category')
+    list_per_page = 20
+
+    search_fields = ('code', 'name', 'product_category')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'code', 'name', 'description', 'price', 'stock', 'unit', 'product_category'
+            ),
+        }),
+    )
