5c9fb7e14b3e584d51c75a2421fb0269b6e40147
Gabriel Cavalheiro Barreto <gabrielcavalheiro@locus.software>
2021-11-05 21:25:57 -0300

    Refactor: alterando o idioma do sistema para inglês.

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-es'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
